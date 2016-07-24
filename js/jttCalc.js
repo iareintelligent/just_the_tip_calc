f3e7640868f64ba279778490969bd8f80a1b9043
Topher Sikorra <iareintelligent@gmail.com>
2016-07-24 15:41:38 -0400

    buttons now work, changed 'blur' to 'change'

--- js/jttCalc.js
+++ js/jttCalc.js
@@ -1,17 +1,24 @@
+// Calculator Variables
 var eBill = document.getElementById("bill"),
     ePercent_tip = document.getElementById("percent_tip"),
-    eDenominator = document.getElementById("denominator"),
+    eNumPeeps = document.getElementById("numPeeps"),
     eTip_amount = document.getElementById("tip_amount"),
     eTotal = document.getElementById("total"),
     tip_amount = 0,
-    total = 0;
+    total = 0
+
+// +/- Button Variables
+var numButton = document.getElementsByClassName("btn-number");
+// debugger;
+
 
 // Default values
 eBill.value = eBill.placeholder;
 ePercent_tip.value = ePercent_tip.placeholder;
-eDenominator.value = eDenominator.placeholder;
+eNumPeeps.value = eNumPeeps.placeholder;
 
-function recalculating() {
+function calculate() {
+    console.log('recalculating');
     var bill = eBill.value;
 
     // get tip value
@@ -22,34 +29,50 @@ function recalculating() {
     }
 
     // number of people is always 1 or greater
-    if (eDenominator.value == 0) {
-        eDenominator.value = 1;
+    if (eNumPeeps.value == 0) {
+        eNumPeeps.value = 1;
     }
 
     // number of people
-    if (eDenominator.value != 0) {
-        var denominator = eDenominator.value;
+    if (eNumPeeps.value != 0) {
+        var numPeeps = eNumPeeps.value;
     } else {
-        var denominator = eDenominator.placeholder;
+        var numPeeps = eNumPeeps.placeholder;
     }
 
     // tip total
-    tip_amount = bill * percent_tip / denominator;
+    tip_amount = bill * percent_tip / numPeeps;
 
     // total to bill
-    total = bill / denominator + tip_amount;
+    total = bill / numPeeps + tip_amount;
 
     if (total > 0) {
         eTip_amount.innerHTML = "$" + tip_amount.toFixed(2);
         eTotal.innerHTML = "$" + total.toFixed(2);
     }
 
-    if (denominator > 1) {
+    if (numPeeps > 1) {
         eTip_amount.innerHTML = eTip_amount.innerHTML + "/per person";
         eTotal.innerHTML = eTotal.innerHTML + "/per person";
     }
 }
 
-eBill.addEventListener("blur", recalculating);
-percent_tip.addEventListener("blur", recalculating);
-denominator.addEventListener("blur", recalculating);
+$('.xcrement').each(function() {
+  var input = $(this).find('input[type="number"]'),
+      plus = $(this).find('button[class="btn btn-default btn-number plus"]'),
+      minus = $(this).find('button[class="btn btn-default btn-number minus"]');
+
+      plus.click(function() {
+        input[0].value++;
+        calculate();
+      });
+
+      minus.click(function() {
+        input[0].value--;
+        calculate();
+      });
+})
+
+eBill.addEventListener("change", calculate);
+percent_tip.addEventListener("change", calculate);
+numPeeps.addEventListener("change", calculate);
